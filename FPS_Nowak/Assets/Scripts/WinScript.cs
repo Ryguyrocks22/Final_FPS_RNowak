a2d13bb0a08cc87a9146b598c74a171687714534
Ryguyrocks22 <70034410+Ryguyrocks22@users.noreply.github.com>
2021-04-28 10:54:43 -0400

    build

---
+++ FPS_Nowak/Assets/Scripts/WinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Winner")
+        {
+            SceneManager.LoadScene(4);
+        }
+
+    }
+}
